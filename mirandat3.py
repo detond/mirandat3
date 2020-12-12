433a51cdc910babf1d67f517308850ca49a4ad58
Vlad Nikiforov <vlad.bfly@gmail.com>
2020-12-12 22:20:50 +0200

    Run 2to3 (broken)
    Initial run of 2t3, the script is broken now. Will be brushed up in the
    next commits.

--- mirandat3.py
+++ mirandat3.py
@@ -21,6 +21,7 @@
 import os, sys
 from struct import unpack
 from datetime import datetime
+from functools import reduce
 
 CONTACT_SIG = 0x43DECADE
 MODULENAME_SIG = 0x4DDECADE
@@ -35,19 +36,19 @@ EventType_File = 1002
 
 def deunicode(txt):
     try:
-        return unicode(txt, encoding='utf-8').encode('utf-8')
+        return str(txt, encoding='utf-8').encode('utf-8')
     except:
         return repr(txt)
 
 def deunicode(txt):
     try:
-        return unicode(txt, encoding='utf-16').encode('utf-8')
+        return str(txt, encoding='utf-16').encode('utf-8')
     except:
         return repr(txt)
 
 def delatin(txt):
     try:
-        return unicode(txt, encoding='latin').encode('utf-8')
+        return str(txt, encoding='latin').encode('utf-8')
     except:
         return repr(txt)
 
@@ -79,7 +80,7 @@ class DBEvent(object):
         self.blob = dat[bloboffs:(bloboffs+self.blobsize-1)]
 
         if self.signature != EVENT_SIG:
-            print "Not a valid event:", self.signature
+            print(("Not a valid event:", self.signature))
 
     def name(self):
         return self.contact.name
@@ -209,7 +210,7 @@ class DBContact(object):
         self.timestampFirstUnread = sig[7]
 
         if self.signature != CONTACT_SIG:
-            print "Not a valid contact:", self.signature
+            print(("Not a valid contact:", self.signature))
 
         self.settings = self._read_settings(dat)
 
@@ -256,7 +257,7 @@ class DBContact(object):
             i = s.next
             settings.append(s.settings)
 
-        settings = reduce(lambda x,y: dict(x.items() + y.items()), settings)
+        settings = reduce(lambda x,y: dict(list(x.items()) + list(y.items())), settings)
         return settings
 
     def _read_events(self, dat):
@@ -271,7 +272,7 @@ class DBContact(object):
     def __str__(self):
         s = "Contact:\n"
         s += "Event count: " + str(self.eventCount) + "\n"
-        for k,v in self.settings.iteritems():
+        for k,v in list(self.settings.items()):
             s += ("%12s:\t%12s\n"%(str(k), str(v)))
         return s
 
@@ -326,15 +327,15 @@ def sqlite3_export(header, dat):
     next_contact = header.firstContact
     while next_contact != 0:
         c = DBContact(dat, next_contact)
-        cur.execute("insert into contacts(name) values (?)", (unicode(str(c.name), 'utf-8'),));
+        cur.execute("insert into contacts(name) values (?)", (str(str(c.name), 'utf-8'),));
         c_id = None
         for row in cur.execute('select last_insert_rowid()'):
-            print row
+            print(row)
             c_id = row[0]
         cur.executemany("insert into settings(owner, name, value) values (?, ?, ?)",
-                        [(c_id, unicode(k, 'utf-8'), unicode(str(v), 'utf-8')) for k, v in c.settings.iteritems()])
+                        [(c_id, str(k, 'utf-8'), str(str(v), 'utf-8')) for k, v in list(c.settings.items())])
         cur.executemany("insert into events(owner, timestamp, type, data) values (?, ?, ?, ?)",
-                        [(c_id, e.timestamp, unicode(e.typestr(), 'utf-8'), unicode(e.parse_blob(), 'utf-8')) for e in c.events])
+                        [(c_id, e.timestamp, str(e.typestr(), 'utf-8'), str(e.parse_blob(), 'utf-8')) for e in c.events])
         next_contact = c.next
 
     con.commit()
@@ -351,7 +352,7 @@ def main():
     (options, args) = parser.parse_args()
 
     if len(args) == 0 or options.filename is None:
-        print "Missing arguments."
+        print("Missing arguments.")
         return
 
     dat = None
@@ -363,7 +364,7 @@ def main():
         next_contact = header.firstContact
         while next_contact != 0:
             c = DBContact(dat, next_contact)
-            print "%12s\t%12s\t%12s\t%12s"%(c.uin, c.nick, c.firstName, c.lastName)
+            print(("%12s\t%12s\t%12s\t%12s"%(c.uin, c.nick, c.firstName, c.lastName)))
             next_contact = c.next
     elif args[0] == "find_contact":
         by = args[1]
@@ -372,25 +373,25 @@ def main():
         while next_contact != 0:
             c = DBContact(dat, next_contact)
             if str(c.settings.get(by)) == val:
-                print c
+                print(c)
             next_contact = c.next
     elif args[0] == "contacts":
         next_contact = header.firstContact
         while next_contact != 0:
             c = DBContact(dat, next_contact)
-            print c
+            print(c)
             next_contact = c.next
     elif args[0] == "ls":
         next_contact = header.firstContact
         while next_contact != 0:
             c = DBContact(dat, next_contact)
             for e in c.events:
-                print e
+                print(e)
             next_contact = c.next
     elif args[0] == "export":
         sqlite3_export(header, dat)
     else:
-        print "Unknown command"
+        print("Unknown command")
 
 if __name__=="__main__":
     main()
